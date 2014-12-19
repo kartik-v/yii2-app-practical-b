c85ff8fa255339833ea489c946086de59ca9125c
Kartik Visweswaran <kartikv2@gmail.com>
2014-12-19 14:08:25 +0530

    Update to release v1.6.0

--- config/web.php
+++ config/web.php
@@ -3,7 +3,7 @@
 $params = require(__DIR__ . '/params.php');
 
 $config = [
-    'id' => 'practicalB',
+    'id' => 'app-practical-b',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'components' => [
